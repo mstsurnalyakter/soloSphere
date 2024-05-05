06e05b7665261e0ce9a6ced92118dc2c1b15360e
mstsurnalyakter <surnalyms@gmail.com>
2024-05-05 08:19:15 +0600

    add

--- client/src/pages/Authentication/Login.jsx
+++ client/src/pages/Authentication/Login.jsx
@@ -4,8 +4,8 @@ import logo from '../../assets/images/logo.png'
 
 const Login = () => {
   return (
-    <div className="flex justify-center items-center min-h-[calc(100vh-306px)]">
-      <div className="flex w-full max-w-sm mx-auto overflow-hidden bg-white rounded-lg shadow-lg  lg:max-w-4xl ">
+    <div className="flex justify-center  items-center min-h-[calc(100vh-306px)]">
+      <div className="flex w-full border max-w-sm mx-auto overflow-hidden bg-white rounded-lg shadow-lg  lg:max-w-4xl ">
         <div
           className="hidden bg-cover bg-center lg:block lg:w-1/2"
           style={{
@@ -15,11 +15,7 @@ const Login = () => {
 
         <div className="w-full px-6 py-8 md:px-8 lg:w-1/2">
           <div className="flex justify-center mx-auto">
-            <img
-              className="w-auto h-7 sm:h-8"
-              src={logo}
-              alt=""
-            />
+            <img className="w-auto h-7 sm:h-8" src={logo} alt="" />
           </div>
 
           <p className="mt-3 text-xl text-center text-gray-600 ">
--- client/src/routes/Routes.jsx
+++ client/src/routes/Routes.jsx
@@ -2,12 +2,17 @@ import { createBrowserRouter } from "react-router-dom";
 import Main from "../layout/Main";
 import Login from "../pages/Authentication/Login";
 import Registration from "../pages/Authentication/Registration";
+import Home from "../pages/Home/Home";
 
 const router = createBrowserRouter([
    {
     path:'/',
     element:<Main/>,
     children:[
+      {
+         index:true,
+         element:<Home/>
+      },
       {
          path:"/login",
          element:<Login/>
