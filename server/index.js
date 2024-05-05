52adc06ea4c7280bb2ec873043dae267578f838b
mstsurnalyakter <surnalyms@gmail.com>
2024-05-05 14:11:51 +0600

    data read api create

--- server/index.js
+++ server/index.js
@@ -17,6 +17,42 @@ app.use(express.json());
 app.use(cors(corsOptions))
 
 
+
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.jimwvxl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+console.log(uri);
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+
+    const jobCollection = client.db("soloSphere").collection("jobs")
+
+    app.get("/jobs", async(req,res)=>{
+        const result = await jobCollection.find().toArray();
+        res.send(result);
+    })
+
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+  }
+}
+run().catch(console.dir);
+
+
+
+
+
 app.get("/",(req,res)=>{
     res.send("I am from Home route")
 })
