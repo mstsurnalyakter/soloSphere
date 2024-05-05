e65c1605b921951a92cfc589eccee379f93be50d
mstsurnalyakter <surnalyms@gmail.com>
2024-05-05 07:36:47 +0600

    firebase setup

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { RouterProvider } from "react-router-dom";
+import router from "./routes/Routes";
+import AuthProvider from "./provider/AuthProvider";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-  <h1>SoloSphere</h1>
-  </React.StrictMode>,
-)
+    <AuthProvider>
+      <RouterProvider router={router}></RouterProvider>
+    </AuthProvider>
+  </React.StrictMode>
+);
