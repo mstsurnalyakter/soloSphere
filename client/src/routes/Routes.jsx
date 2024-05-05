60c1851d412a6c264058917b022f0972a76d460f
mstsurnalyakter <surnalyms@gmail.com>
2024-05-05 16:43:56 +0600

    get single data

--- client/src/routes/Routes.jsx
+++ client/src/routes/Routes.jsx
@@ -3,27 +3,32 @@ import Main from "../layout/Main";
 import Login from "../pages/Authentication/Login";
 import Registration from "../pages/Authentication/Registration";
 import Home from "../pages/Home/Home";
+import JobDetails from "../pages/JobDetails/JobDetails";
 
 const router = createBrowserRouter([
-   {
-    path:'/',
-    element:<Main/>,
-    children:[
+  {
+    path: "/",
+    element: <Main />,
+    children: [
       {
-         index:true,
-         element:<Home/>
+        index: true,
+        element: <Home />,
       },
       {
-         path:"/login",
-         element:<Login/>
+        path: "/login",
+        element: <Login />,
       },
       {
-         path:"/registration",
-         element:<Registration/>
-      }
-    ]
-   }
-])
+        path: "/registration",
+        element: <Registration />,
+      },
+      {
+        path: "`/job/:id`",
+        element:<JobDetails/>
+      },
+    ],
+  },
+]);
 
 
 export default router;
