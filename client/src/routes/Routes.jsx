82a9beab892b98ed738b6eec919222085030e41b
mstsurnalyakter <surnalyms@gmail.com>
2024-05-05 08:04:01 +0600

    Login adn Registration page add

--- client/src/routes/Routes.jsx
+++ client/src/routes/Routes.jsx
@@ -1,12 +1,21 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../layout/Main";
+import Login from "../pages/Authentication/Login";
+import Registration from "../pages/Authentication/Registration";
 
 const router = createBrowserRouter([
    {
     path:'/',
     element:<Main/>,
     children:[
-        
+      {
+         path:"/login",
+         element:<Login/>
+      },
+      {
+         path:"/registration",
+         element:<Registration/>
+      }
     ]
    }
 ])
