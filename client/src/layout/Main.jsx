ec9f5a7692a8d99bf32607518d5aeef82c823b74
mstsurnalyakter <surnalyms@gmail.com>
2024-05-05 07:44:36 +0600

    Navbar and Footer component add

--- client/src/layout/Main.jsx
+++ client/src/layout/Main.jsx
@@ -1,11 +1,15 @@
 import { Outlet } from "react-router-dom"
+import Navbar from "../pages/shared/Navbar"
+import Footer from "../pages/shared/Footer"
 
 
 const Main = () => {
   return (
-    <div>
-        <Outlet/>
-    </div>
+    <>
+    <Navbar/>
+    <Outlet/>
+    <Footer/>
+    </>
   )
 }
 
