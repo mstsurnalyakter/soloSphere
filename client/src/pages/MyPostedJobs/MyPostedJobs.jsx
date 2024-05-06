2722586525d127e4db5aeb83339c39f17a33817f
mstsurnalyakter <surnalyms@gmail.com>
2024-05-06 11:40:10 +0600

    Delete job functionally add

--- client/src/pages/MyPostedJobs/MyPostedJobs.jsx
+++ client/src/pages/MyPostedJobs/MyPostedJobs.jsx
@@ -1,5 +1,8 @@
 import Spinner from "../../components/Spinner";
 import useMyPostedJobs from "../../hooks/useMyPostedJobs";
+import { Link } from "react-router-dom";
+import Swal from "sweetalert2";
+import toast from "react-hot-toast";
 
 
 
@@ -12,6 +15,36 @@ if (isLoading) {
   return <div className="mt-10 flex items-center justify-center">
     <Spinner/>
   </div>
+}
+
+const handleDelete = id =>{
+  Swal.fire({
+    title: "Are you sure?",
+    text: "You won't be able to revert this!",
+    icon: "warning",
+    showCancelButton: true,
+    confirmButtonColor: "#3085d6",
+    cancelButtonColor: "#d33",
+    confirmButtonText: "Yes, delete it!",
+  }).then((result) => {
+    if (result.isConfirmed) {
+      fetch(`${import.meta.env.VITE_API_URL}/jobs/${id}`, {
+        method: "DELETE",
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          if (data.deletedCount > 0) {
+            Swal.fire({
+              title: "Deleted!",
+              text: "Job has been deleted.",
+              icon: "success",
+            });
+            refetch();
+          }
+        })
+        .catch((error) => toast.error(error.message));
+    }
+  });
 }
 
   return (
@@ -117,7 +150,7 @@ if (isLoading) {
                           </td>
                           <td className="px-4 py-4 text-sm whitespace-nowrap">
                             <div className="flex items-center gap-x-6">
-                              <button className="text-gray-500 transition-colors duration-200   hover:text-red-500 focus:outline-none">
+                              <Link to={`/update/${_id}`} className="text-gray-500 transition-colors duration-200   hover:text-red-500 focus:outline-none">
                                 <svg
                                   xmlns="http://www.w3.org/2000/svg"
                                   fill="none"
@@ -132,9 +165,9 @@ if (isLoading) {
                                     d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
                                   />
                                 </svg>
-                              </button>
+                              </Link>
 
-                              <button className="text-gray-500 transition-colors duration-200   hover:text-yellow-500 focus:outline-none">
+                              <button onClick={()=>handleDelete(_id)} className="text-gray-500 transition-colors duration-200   hover:text-yellow-500 focus:outline-none">
                                 <svg
                                   xmlns="http://www.w3.org/2000/svg"
                                   fill="none"
